7cb7cc877cebcfe9025635d655169fa13a8454ea
jmcelwain <jmcelwain@gmail.com>
2017-07-05 16:20:54 -0500

    4: COPY

--- src/4_file_io/copy.c
+++ src/4_file_io/copy.c
@@ -13,7 +13,37 @@ int main (int argc, char *argv[])
   ssize_t numRead;
   char buf[BUF_SIZE];
 
-  if (argc != 3 || strcmp(argv[1], "--help") == 0) {
-    usageErr("%s old-file new-file \n", arv[0]);
-  }
+  if (argc != 3 || strcmp(argv[1], "--help") == 0)
+    usageErr("%s old-file new-file \n", argv[0]);
+
+  /* open src and dst files */
+
+  inputFd = open(argv[1], O_RDONLY);
+  if (inputFd == -1)
+    errExit("error opening file %s", argv[1]);
+
+  openFlags = O_CREAT | O_WRONLY | O_TRUNC;
+  filePerms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP |
+    S_IROTH | S_IWOTH; /* rw-rw-rw- */
+
+  outputFd = open(argv[2], openFlags, filePerms);
+
+  if (outputFd == -1)
+    errExit("error opening file %s", argv[2]);
+
+  /* move data until EOF or error */
+  while ((numRead = read(inputFd, buf, BUF_SIZE)) > 0)
+    if (write(outputFd, buf, numRead) != numRead)
+      fatal("couldn't write whole buffer");
+
+  if (numRead == -1)
+    errExit("read");
+
+  if (close(inputFd) == -1)
+    errExit("close input");
+
+  if (close(outputFd) == -1)
+    errExit("close output");
+
+  exit(EXIT_SUCCESS);
 }
